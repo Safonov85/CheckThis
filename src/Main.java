517ae2ab75a9d1edd7f81ea00d504dcc4d9faad4
Safonov85 <safonov85@yahoo.com>
2017-05-05 23:50:19 +0200

    Uncomplete alarm clock

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 import java.text.DecimalFormat;
 import java.util.Scanner;
 import java.lang.String;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.lang.Thread;
 
 public class Main
 {
@@ -49,169 +52,187 @@ public class Main
 	static double pay;
 	static double tax;
 	static double finalPay;
+	
+	// Alarm Clock
+	static int countDown;
+	static Timer timer;
 
 	
-	public static void main(String[] args)
+	public static void main(String[] args) throws Exception
 	{	
 		CalculateStuff();
 	}
 	
-	static void CalculateStuff()
+	static void CalculateStuff() 
 	{
 		Scanner reader = new Scanner(System.in);
+//		System.out.println("");
+//		System.out.println("---- PI Decimal Places ----");
+//		System.out.println("How many decimals of PI: ");
+//		
+//		int amount = reader.nextInt();
+//		
+//		// adds the decimals("#") amount put in by the user to the variable
+//		int i = 0;
+//		while(i < amount)
+//		{
+//			decimals = decimals + "#";
+//			i++;
+//		}
+//		
+//		DecimalFormat decimalFormat = new DecimalFormat(decimals); // #.#### <--- 4 decimals
+//		
+//		System.out.println("PI decimals : " + decimalFormat.format(PI));
+//		System.out.println("");
+//		System.out.println("---- Fibonacci Sequence ----");
+//		System.out.print("Pls enter how many Fibonacci numbers you want: ");
+//		fiboAmount = reader.nextInt();
+//		
+//		for(int j = 0; j < fiboAmount; j++)
+//		{
+//			if(j < 2)
+//			{
+//				System.out.print(secondNum + ", ");
+//				secondNum = 1;
+//			}
+//			else
+//			{
+//				lastNum = firstNum + secondNum;
+//				System.out.print(lastNum + ", ");
+//				firstNum = secondNum;
+//				secondNum = lastNum;
+//			}
+//		}
+//		
+//		System.out.println("");
+//		System.out.println("");
+//		System.out.println("---- Prime Factorization ----");
+//		System.out.print("Enter a number to see if it's Prime: ");
+//		primeInput = reader.nextInt();
+//		
+//		for(int ij = 1; ij < 10; ij++)
+//		{
+//			primeResult = primeInput / (double)ij;
+//			
+//			if((primeResult % 1) == 0 && primeResult != 1.0)
+//			{
+//				if(primeResult == primeInput)
+//				{
+//					System.out.println((int)primeResult);
+//					isPrime = true;
+//				}
+//				else
+//				{
+//					isPrime = false;
+//				}
+//			}
+//		}
+//		
+//		System.out.println(isPrime);
+//		
+//		System.out.println("");
+//		System.out.println("");
+//		System.out.println("---- Cost of white Ceramic Tile ----");
+//		System.out.println("1m square cost 200kr");
+//		System.out.print("How many Square meters do you want to buy? : ");
+//		squareInput = reader.nextDouble();
+//		
+//		costResult = squareInput * tileCost;
+//		
+//		System.out.println("Your cost will be " + costResult + " kr");
+//		
+//		System.out.println("");
+//		System.out.println("");
+//		System.out.println("---- Mortgage Calculator ----");
+//		System.out.print("Put in annual intrest rate: ");
+//		interestYear = reader.nextDouble();
+//		interestMonth = interestYear / 1200;
+//		System.out.print("Put in over how many years you want to pay this: ");
+//		years = reader.nextInt();
+//		System.out.print("How much is the loan? : ");
+//		amountLoan = reader.nextDouble();
+//		paymentPerMonth = amountLoan * interestMonth / (1-1 / Math.pow(1 + interestMonth, years * 12));
+//		
+//		System.out.println("You will have to pay " + paymentPerMonth + " per month for " + years + " years.");
+//		
+//		System.out.println("");
+//		System.out.println("");
+//		System.out.println("---- Change Return ----");
+//		System.out.print("How much does it cost: ");
+//		cost = reader.nextDouble();
+//		System.out.print("How much money are you giving? : ");
+//		myMoney = reader.nextDouble();
+//		if(cost > myMoney)
+//		{
+//			System.out.println("Sorry. You don't have enough money.");
+//		}
+//		else
+//		{
+//			moneyReturn = myMoney - cost;
+//			System.out.println("And here is " + moneyReturn + " back to you.");
+//		}
+//		
+//		System.out.println("");
+//		System.out.println("");
+//		System.out.println("---- Decimal/Binary Conversion ----");
+//		System.out.print("Enter decimal Number: ");
+//		decimal = reader.nextInt();
+//		System.out.println("Binary for that decimal is: " + Integer.toString(decimal,2));
+//		
+//		System.out.println("");
+//		System.out.println("");
+//		System.out.println("---- Simple Calc ----");
+//		System.out.print("Type first Number: ");
+//		firstNumber = reader.nextDouble();
+//		System.out.print("Type in Operator ex. / * + - : ");
+//		operator = reader.next();
+//		System.out.print("Type second Number: ");
+//		secondNumber = reader.nextDouble();
+//		
+//		if(operator.contains("/"))
+//		{
+//			resultNumber = firstNumber / secondNumber;
+//		}
+//		if(operator.contains("*"))
+//		{
+//			resultNumber = firstNumber * secondNumber;
+//		}
+//		if(operator.contains("+"))
+//		{
+//			resultNumber = firstNumber + secondNumber;
+//		}
+//		if(operator.contains("-"))
+//		{
+//			resultNumber = firstNumber - secondNumber;
+//		}
+//		
+//		System.out.println(firstNumber + " " + operator + " " + secondNumber + " is " + resultNumber);
+//		
+//		System.out.println("");
+//		System.out.println("");
+//		System.out.println("---- Your Taxes ----");
+//		System.out.print("Enter How much you earn per month: ");
+//		pay = reader.nextDouble();
+//		System.out.print("Enter Tax amount in your area: ");
+//		tax = reader.nextDouble();
+//		
+//		finalPay = pay - (pay * (tax * 0.01));
+//		
+//		System.out.print("Your final pay will be " + finalPay + "kr after taxes.");
+		
 		System.out.println("");
-		System.out.println("---- PI Decimal Places ----");
-		System.out.println("How many decimals of PI: ");
-		
-		int amount = reader.nextInt();
-		
-		// adds the decimals("#") amount put in by the user to the variable
-		int i = 0;
-		while(i < amount)
+		System.out.println("");
+		System.out.println("---- Alarm Clock ----");
+		System.out.print("In how many seconds should Alarm ring? : ");
+		countDown = reader.nextInt();
+		countDown = countDown * 1000;
+		//timer = new Timer(countDown, );
+		int counting = 0;
+		while(countDown > counting)
 		{
-			decimals = decimals + "#";
-			i++;
+			Thread.sleep(1000);
+			countDown -= 1000;
 		}
-		
-		DecimalFormat decimalFormat = new DecimalFormat(decimals); // #.#### <--- 4 decimals
-		
-		System.out.println("PI decimals : " + decimalFormat.format(PI));
-		System.out.println("");
-		System.out.println("---- Fibonacci Sequence ----");
-		System.out.print("Pls enter how many Fibonacci numbers you want: ");
-		fiboAmount = reader.nextInt();
-		
-		for(int j = 0; j < fiboAmount; j++)
-		{
-			if(j < 2)
-			{
-				System.out.print(secondNum + ", ");
-				secondNum = 1;
-			}
-			else
-			{
-				lastNum = firstNum + secondNum;
-				System.out.print(lastNum + ", ");
-				firstNum = secondNum;
-				secondNum = lastNum;
-			}
-		}
-		
-		System.out.println("");
-		System.out.println("");
-		System.out.println("---- Prime Factorization ----");
-		System.out.print("Enter a number to see if it's Prime: ");
-		primeInput = reader.nextInt();
-		
-		for(int ij = 1; ij < 10; ij++)
-		{
-			primeResult = primeInput / (double)ij;
-			
-			if((primeResult % 1) == 0 && primeResult != 1.0)
-			{
-				if(primeResult == primeInput)
-				{
-					System.out.println((int)primeResult);
-					isPrime = true;
-				}
-				else
-				{
-					isPrime = false;
-				}
-			}
-		}
-		
-		System.out.println(isPrime);
-		
-		System.out.println("");
-		System.out.println("");
-		System.out.println("---- Cost of white Ceramic Tile ----");
-		System.out.println("1m square cost 200kr");
-		System.out.print("How many Square meters do you want to buy? : ");
-		squareInput = reader.nextDouble();
-		
-		costResult = squareInput * tileCost;
-		
-		System.out.println("Your cost will be " + costResult + " kr");
-		
-		System.out.println("");
-		System.out.println("");
-		System.out.println("---- Mortgage Calculator ----");
-		System.out.print("Put in annual intrest rate: ");
-		interestYear = reader.nextDouble();
-		interestMonth = interestYear / 1200;
-		System.out.print("Put in over how many years you want to pay this: ");
-		years = reader.nextInt();
-		System.out.print("How much is the loan? : ");
-		amountLoan = reader.nextDouble();
-		paymentPerMonth = amountLoan * interestMonth / (1-1 / Math.pow(1 + interestMonth, years * 12));
-		
-		System.out.println("You will have to pay " + paymentPerMonth + " per month for " + years + " years.");
-		
-		System.out.println("");
-		System.out.println("");
-		System.out.println("---- Change Return ----");
-		System.out.print("How much does it cost: ");
-		cost = reader.nextDouble();
-		System.out.print("How much money are you giving? : ");
-		myMoney = reader.nextDouble();
-		if(cost > myMoney)
-		{
-			System.out.println("Sorry. You don't have enough money.");
-		}
-		else
-		{
-			moneyReturn = myMoney - cost;
-			System.out.println("And here is " + moneyReturn + " back to you.");
-		}
-		
-		System.out.println("");
-		System.out.println("");
-		System.out.println("---- Decimal/Binary Conversion ----");
-		System.out.print("Enter decimal Number: ");
-		decimal = reader.nextInt();
-		System.out.println("Binary for that decimal is: " + Integer.toString(decimal,2));
-		
-		System.out.println("");
-		System.out.println("");
-		System.out.println("---- Simple Calc ----");
-		System.out.print("Type first Number: ");
-		firstNumber = reader.nextDouble();
-		System.out.print("Type in Operator ex. / * + - : ");
-		operator = reader.next();
-		System.out.print("Type second Number: ");
-		secondNumber = reader.nextDouble();
-		
-		if(operator.contains("/"))
-		{
-			resultNumber = firstNumber / secondNumber;
-		}
-		if(operator.contains("*"))
-		{
-			resultNumber = firstNumber * secondNumber;
-		}
-		if(operator.contains("+"))
-		{
-			resultNumber = firstNumber + secondNumber;
-		}
-		if(operator.contains("-"))
-		{
-			resultNumber = firstNumber - secondNumber;
-		}
-		
-		System.out.println(firstNumber + " " + operator + " " + secondNumber + " is " + resultNumber);
-		
-		System.out.println("");
-		System.out.println("");
-		System.out.println("---- Your Taxes ----");
-		System.out.print("Enter How much you earn per month: ");
-		pay = reader.nextDouble();
-		System.out.print("Enter Tax amount in your area: ");
-		tax = reader.nextDouble();
-		
-		finalPay = pay - (pay * (tax * 0.01));
-		
-		System.out.print("Your final pay will be " + finalPay + "kr after taxes.");
 		
 		reader.close();
 	}
